8003bb99b0d1380adb1f0cc25e00c4b523386acc
cyb <838182202@qq.com>
2020-03-12 21:04:29 +0800

    第一次提交

---
+++ src/components/button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="tls-button">
+      第一个按钮
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tlsButton',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import button from './components/button.vue'
+const componentArr = [button]
+componentArr.forEach(item => {
+    Vue.component(item.name, item)
+})
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
